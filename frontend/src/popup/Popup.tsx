8a57b761c90910d587924098e15cd8d11e4ea5e5
jamesjmnz <mysterysign123@gmail.com>
2025-12-12 02:08:34 +0800

    initialized extension popup

---
+++ frontend/src/popup/Popup.tsx
@@ -0,0 +1,11 @@
+
+const Popup = () => {
+  return (
+    <div className="w-[340px] min-h-[260px] bg-slate-950 text-slate-50 p-4">
+        <h1>Fake News Detector</h1>
+        <button>Test</button>
+    </div>
+  )
+}
+
+export default Popup
