6ec873a297dbf9de1b32697f11b1f5046e72c47e
jamesjmnz <mysterysign123@gmail.com>
2026-01-04 18:41:52 +0800

    fix(auth): root env access to google client id and secret id

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -6,4 +6,14 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    baseURL: process.env.BETTER_AUTH_URL,
+    emailAndPassword: { 
+        enabled: true, 
+      }, 
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
