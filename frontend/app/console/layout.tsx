1112a7cdd554624ff79e7bee5920b9961bce85d0
jamesjmnz <mysterysign123@gmail.com>
2026-01-07 22:18:09 +0800

    feat: added protected route on /console

--- frontend/app/console/layout.tsx
+++ frontend/app/console/layout.tsx
@@ -1,15 +1,26 @@
 // app/console/layout.tsx
 import ConsoleSidebar from "@/components/layout/ConsoleSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
   
 
 
-export default function ConsoleLayout({
+
+export default async function ConsoleLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const headersList = await headers()
+  const session = await auth.api.getSession({headers: headersList})
+
+  if (!session?.user?.id) {
+    redirect("/login")
+  }
+
   return (
     <div className="flex min-h-screen">
       <SidebarProvider>
